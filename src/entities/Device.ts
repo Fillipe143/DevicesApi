80fcefa0fa0190f629b3c5cec4f5d0f023025c79
Fillipe143 <fillipedev.contato@gmail.com>
2023-06-18 20:58:11 -0300

    Refined QueryDevice

--- src/entities/Device.ts
+++ src/entities/Device.ts
@@ -1,10 +1,13 @@
 import { z } from "zod";
 
+const MAC_LENGTH = 17;
+const NUMBER_OF_CHANNELS = 13;
+
 const QueryDevice = z.object({
     ssid: z.string(),
-    mac: z.string(),
-    rssi: z.string().transform(val => Number(val)).refine(val => !isNaN(val)),
-    channel: z.string().transform(val => Number(val)).refine(val => !isNaN(val))
+    mac: z.string().refine(val => val.length == MAC_LENGTH),
+    rssi: z.string().transform(val => Number(val)).refine(val => !isNaN(val) && val <= 0),
+    channel: z.string().transform(val => Number(val)).refine(val => !isNaN(val) && val >= 1 && val <= NUMBER_OF_CHANNELS)
 });
 
 type Device = typeof QueryDevice._type;
