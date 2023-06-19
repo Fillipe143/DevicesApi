ad8729bce8d24554d719ee6e520e592ca448bf2d
Fillipe143 <fillipedev.contato@gmail.com>
2023-06-18 21:19:57 -0300

    Created AddDeviceRoute

--- src/routes/AddDeviceRoute.ts
+++ src/routes/AddDeviceRoute.ts
@@ -1,9 +1,21 @@
 import { Request, Response } from "express";
 import { Route } from "./Route";
+import { QueryDevice } from "../entities";
+import { deviceRepository } from "../repositories";
 
 class AddDeviceRoute implements Route {
-    async home(req: Request, res: Response): Promise<void> {
-        res.status(200).json({ message: "Successful" });
+    public async home(req: Request, res: Response): Promise<void> {
+        const device = QueryDevice.safeParse(req.query);
+
+        if (device.success) {
+            deviceRepository.add(device.data)
+                .then(() => AddDeviceRoute.result(res, 200, "Successful"))
+                .catch(() => AddDeviceRoute.result(res, 500, "Internal Server Error"));
+        } else AddDeviceRoute.result(res, 400, "Bad request");
+    }
+
+    private static result(res: Response, status: number, message: string): void {
+        res.status(status).json({ status, message });
     }
 }
 
