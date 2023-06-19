9b8f05bdb59b8a1305bf82a21dd4d07b62527a10
Fillipe143 <fillipedev.contato@gmail.com>
2023-06-19 09:58:21 -0300

    QueryDeviceFilter parameters no longer accept null type

--- src/entities/DeviceFilter.ts
+++ src/entities/DeviceFilter.ts
@@ -2,18 +2,21 @@ import { z } from "zod";
 import { MAC_LENGTH, NUMBER_OF_CHANNELS } from "./Device";
 
 const QueryDeviceFilter = z.object({
-    id: z.string().transform(val => Number(val)).refine(val => !isNaN(val)).optional().nullable(),
+    id: z.string().transform(val => Number(val)).refine(val => !isNaN(val)).optional(),
 
-    ssid: z.string().optional().nullable(),
-    mac: z.string().refine(val => val.length == MAC_LENGTH).optional().nullable(),
+    ssid: z.string().optional(),
+    mac: z.string().refine(val => val.length == MAC_LENGTH).optional(),
 
-    min_rssi: z.string().transform(val => Number(val)).refine(val => !isNaN(val) && val <= 0).optional().nullable(),
-    max_rssi: z.string().transform(val => Number(val)).refine(val => !isNaN(val) && val <= 0).optional().nullable(),
+    rssi: z.string().transform(val => Number(val)).refine(val => !isNaN(val) && val <= 0).optional(),
+    min_rssi: z.string().transform(val => Number(val)).refine(val => !isNaN(val) && val <= 0).optional(),
+    max_rssi: z.string().transform(val => Number(val)).refine(val => !isNaN(val) && val <= 0).optional(),
 
-    channel: z.string().transform(val => Number(val)).refine(val => !isNaN(val) && val >= 1 && val <= NUMBER_OF_CHANNELS).optional().nullable(),
-
-    createdAfter: z.string().transform(val => Number(val)).refine(val => !isNaN(val)).optional().nullable(),
-    createdBefore: z.string().transform(val => Number(val)).refine(val => !isNaN(val)).optional().nullable()
+    channel: z.string().transform(val => Number(val)).refine(val => !isNaN(val) && val >= 1 && val <= NUMBER_OF_CHANNELS).optional(),
+    min_channel: z.string().transform(val => Number(val)).refine(val => !isNaN(val) && val >= 1 && val <= NUMBER_OF_CHANNELS).optional(),
+    max_channel: z.string().transform(val => Number(val)).refine(val => !isNaN(val) && val >= 1 && val <= NUMBER_OF_CHANNELS).optional(),
+    
+    createdAfter: z.string().transform(val => Number(val)).refine(val => !isNaN(val)).optional(),
+    createdBefore: z.string().transform(val => Number(val)).refine(val => !isNaN(val)).optional()
 });
 
 type DeviceFilter = typeof QueryDeviceFilter._type;
